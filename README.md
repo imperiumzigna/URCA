8fc159157cceb4874fe43d176cf90146745f8d5f
igor <igor@localhost.igor>
2016-12-02 22:17:49 -0300

    versao 1.0.0 urca

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# urca_testes
+# Projeto URCA - UEMA
+
+## Descrição:
+
+.................a fazer
+
+### Componentes
+* Carlos Henrique
+
+* Igor Amorim Silva
+
+* Ana Paula
+
+* Vitor
+
+
+## Requisitos
+
+* Python 2.7 
+* Libfreenect (python)
+* CV2
+* numpy
+* pyocclient
+
+## Instruções de instalação
+ - As instruções de instalações das bibliotecas acima
+ podem ser encontradas na pasta "docs" deste repositório.
+ 
+ 
+## Inicialização do código
+ 
+       cd {path}/URCA
+       sudo python main.py
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+# Written by Igor Amorim Silva ~ 2016
+# Version: 1.0.0
+# -*- coding: utf-8 -*-
+from gpiozero import MotionSensor
+from datetime import datetime
 import freenect
 import cv2
 import numpy as np
 import owncloud
-import time
 
-i=0
-
+# General paths where to save data
+urca_path = 'URCA'
+depth_path = 'URCA/DEPTH/'
+rgb_path = 'URCA/RGB/'
+# Start the PIR sensor on GPIO - 4
+sensor = MotionSensor(4)
+# Get the current date of today
+today = str(datetime.date.today())
+i = 0
+# Conection with owncloud server
 oc = owncloud.Client('http://nuvem.cct.uema.br')
 oc.login('urca', 'KvZC-T5pe-8HmP')
-oc.mkdir('URCA')
-oc.mkdir('URCA/DEPTH')
-oc.mkdir('URCA/RGB')
+oc.mkdir(urca_path)
+
+oc.mkdir(depth_path + today)
+oc.mkdir(rgb_path + today)
+
 # function to get RGB image from kinect
 def get_video():
     array, _ = freenect.sync_get_video()
@@ -38,13 +52,19 @@ if __name__ == "__main__":
         # display depth image
         cv2.imshow('Depth image', depth)
 
+        # If a person is detected print image on file and send to cloud
+        if sensor.motion_detected:
+            time_now = str(datetime.now())
+            cv2.imwrite('../URCA/DEPTH/' + today + '/' + time_now + '.jpg', depth)
+            cv2.imwrite('../URCA/RGB/' + today + '/' + time_now + '.jpg', frame)
+            oc.put_directory('URCA/', 'URCA/')
 
-        cv2.imwrite('../URCA/DEPTH/Depth image' + str(i)+'.jpg' , depth)
-        cv2.imwrite('../URCA/RGB/RGB image' + str(i)+'.jpg' , frame)
-        i=i+1
+        # Test current date for file management issues
+        if today == str(datetime.date.today()):
+            today = str(datetime.date.today())
+            oc.mkdir(depth_path + today)
+            oc.mkdir(rgb_path + today)
 
-        oc.put_directory('URCA/', 'URCA/')
-        time.sleep(120)
         # quit program when 'esc' key is pressed
         k = cv2.waitKey(5) & 0xFF
         if k == 27:
